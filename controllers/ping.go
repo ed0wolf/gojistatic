dcb79afbca2329edcbab5267745b5c575689cde0
Ed0wolf <Ed0wolf@gmail.com>
2014-05-08 00:28:53 +0100

    Adding basic / route

--- controllers/ping.go
+++ controllers/ping.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/zenazn/goji/web"
@@ -9,8 +10,21 @@ import (
 
 func AddRoutes(m *web.Mux) {
 	m.Get("/ping", ping)
+	m.Get("/", index)
+	fmt.Print("Added index")
 }
 
 func ping(c web.C, w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "1.0.0")
+}
+
+func index(c web.C, w http.ResponseWriter, r *http.Request) {
+	content, err := ioutil.ReadFile("./assets/index.html")
+
+	if err != nil {
+		w.WriteHeader(404)
+		return
+	}
+
+	w.Write(content)
 }
